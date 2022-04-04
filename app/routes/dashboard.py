7bedd43864a39753d1d4fa76a2dd7face2b9d0d4
Cameron Quicksall <cameron.quicksall70@gmail.com>
2022-04-04 00:16:56 -0500

    create custom decorator functions to act as route middleware // create api routes for comment, vote, and posts

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
+from app.models import Post
+from app.db import get_db
+from app.utils.auth import login_required
 
 # This time, using the url_prefix argument, we prefix every route in the blueprint with /dashboard.
 # The routes then become /dashboard and /dashboard/edit/<id> when registered with the app.
@@ -7,9 +10,31 @@ from flask import Blueprint, render_template
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
 @bp.route('/')
+@login_required
 def dash():
-    return render_template('dashboard.html')
+    db = get_db()
+    posts = (
+        db.query(Post)
+        .filter(Post.user_id == session.get('user_id'))
+        .order_by(Post.created_at.desc())
+        .all()
+    )
+    return render_template(
+        'dashboard.html',
+        posts = posts,
+        loggedIn=session.get('loggedIn')
+        )
 
 @bp.route('/edit/<id>')
+@login_required
 def edit(id):
-    return render_template('edit-post.html')
+    # get single post by id
+    db = get_db()
+    post = db.query(Post).filter(Post.id == id).one()
+
+    # render edit page
+    return render_template(
+        'edit-post.html',
+        post = post,
+        loggedIn=session.get('loggedIn')
+        )
